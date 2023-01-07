ec716e04216ddd24772227200b8083544c9896e8
tegarrdn <tegarrdn.1112@gmail.com>
2023-01-07 11:52:18 +0700

    first commit

---
+++ qgis2web_2022_12_13-17_11_54_322000/data/puskesmas_1.js
@@ -0,0 +1,29 @@
+var json_puskesmas_1 = {
+"type": "FeatureCollection",
+"name": "puskesmas_1",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "pusk_btg": "Puskesmas Batang 1" }, "geometry": { "type": "Point", "coordinates": [ 109.738556834150359, -6.915382085383976 ] } },
+{ "type": "Feature", "properties": { "pusk_btg": "Puskesmas Batang 2" }, "geometry": { "type": "Point", "coordinates": [ 109.725621967772085, -6.93519719983581 ] } },
+{ "type": "Feature", "properties": { "pusk_btg": "Puskesmas Batang 3" }, "geometry": { "type": "Point", "coordinates": [ 109.715989620469102, -6.892814871702719 ] } },
+{ "type": "Feature", "properties": { "pusk_btg": "Puskesmas Batang 4" }, "geometry": { "type": "Point", "coordinates": [ 109.7374559944586, -6.887310673243877 ] } },
+{ "type": "Feature", "properties": { "pusk_btg": "Puskesmas Kandeman" }, "geometry": { "type": "Point", "coordinates": [ 109.775710173747541, -6.911804356385727 ] } },
+{ "type": "Feature", "properties": { "pusk_btg": "Puskesmas Tulis 1" }, "geometry": { "type": "Point", "coordinates": [ 109.81176267365295, -6.940150978448768 ] } },
+{ "type": "Feature", "properties": { "pusk_btg": "Puskesmas Subah" }, "geometry": { "type": "Point", "coordinates": [ 109.875336165852588, -6.947031226522322 ] } },
+{ "type": "Feature", "properties": { "pusk_btg": "Puskesmas Banyuputih" }, "geometry": { "type": "Point", "coordinates": [ 109.941386547358704, -6.968222390588867 ] } },
+{ "type": "Feature", "properties": { "pusk_btg": "Puskesmas Gringsing 2" }, "geometry": { "type": "Point", "coordinates": [ 109.975787787726475, -6.94813206621409 ] } },
+{ "type": "Feature", "properties": { "pusk_btg": "Puskesmas Gringsing 1" }, "geometry": { "type": "Point", "coordinates": [ 110.031930612006676, -6.953361054749991 ] } },
+{ "type": "Feature", "properties": { "pusk_btg": "Puskesmas Tersono Batang" }, "geometry": { "type": "Point", "coordinates": [ 109.962577711425268, -7.029594203404965 ] } },
+{ "type": "Feature", "properties": { "pusk_btg": "Puskesmas Limpung" }, "geometry": { "type": "Point", "coordinates": [ 109.923497902367487, -7.032071092711446 ] } },
+{ "type": "Feature", "properties": { "pusk_btg": "Puskesmas Pecalungan" }, "geometry": { "type": "Point", "coordinates": [ 109.859098780399023, -7.008127829415479 ] } },
+{ "type": "Feature", "properties": { "pusk_btg": "Puskesmas Bandar 2" }, "geometry": { "type": "Point", "coordinates": [ 109.791672349278187, -7.121514317667644 ] } },
+{ "type": "Feature", "properties": { "pusk_btg": "Puskesmas Bandar 1" }, "geometry": { "type": "Point", "coordinates": [ 109.794699658430559, -7.020237066024934 ] } },
+{ "type": "Feature", "properties": { "pusk_btg": "Puskesmas Wonotunggal" }, "geometry": { "type": "Point", "coordinates": [ 109.756995898987483, -6.997669852343678 ] } },
+{ "type": "Feature", "properties": { "pusk_btg": "Puskesmas Warungasem" }, "geometry": { "type": "Point", "coordinates": [ 109.712687101393797, -6.962718192130025 ] } },
+{ "type": "Feature", "properties": { "pusk_btg": "Puskesmas Blado 2" }, "geometry": { "type": "Point", "coordinates": [ 109.816979481862418, -7.15314216816033 ] } },
+{ "type": "Feature", "properties": { "pusk_btg": "Puskesmas Blado 1" }, "geometry": { "type": "Point", "coordinates": [ 109.847475261775088, -7.066127542809503 ] } },
+{ "type": "Feature", "properties": { "pusk_btg": "Puskesmas Reban" }, "geometry": { "type": "Point", "coordinates": [ 109.880004093681933, -7.086458062751285 ] } },
+{ "type": "Feature", "properties": { "pusk_btg": "Puskesmas Bawang Batang" }, "geometry": { "type": "Point", "coordinates": [ 109.932050224732905, -7.081172127566422 ] } },
+{ "type": "Feature", "properties": { "pusk_btg": "Puskesmas Bawang 1" }, "geometry": { "type": "Point", "coordinates": [ 109.918632081571332, -7.155988440952179 ] } }
+]
+}
